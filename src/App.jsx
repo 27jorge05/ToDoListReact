cdfdf0a047f8112672005d2c61e3307ebcfe41b5
27jorge05 <luisparrachuquimia@gmail.com>
2026-08-31 17:43:40 -0400

    TOD-6: Using getAll in App

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,21 @@
-
+import { useEffect } from "react"
+import { getAll } from "./services/tarea.service"
 
 
 function App() {
+  useEffect(() => {
+    async function loadTasks() {
+      try {
+        const response = await getAll()
+        console.log(response)
+      } catch (error) {
+        console.error(error)
+      }
+
+    }
+    loadTasks()
+  }, [])
+
   return <h1>Hola Mundo</h1>
 }
 
