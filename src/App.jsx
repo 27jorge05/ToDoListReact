13abcb0ae71b31e4ad4ec1a9c8f8711566bd3947
27jorge05 <luisparrachuquimia@gmail.com>
2026-08-31 18:55:19 -0400

    Mounting a beautiful html and css

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,19 @@
 import CategoryList from './components/categories/CategoryList'
+import ThemeToggle from './components/ThemeToggle'
 
 function App() {
-  return <CategoryList />
+  return (
+    <div className="app">
+      <header className="appHeader">
+        <h1 className="appBrand">ToDoList</h1>
+        <ThemeToggle />
+      </header>
+
+      <main className="appContent">
+        <CategoryList />
+      </main>
+    </div>
+  )
 }
 
 export default App
